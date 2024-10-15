2287f97843cec5e548d2e9778a70f41598dac945
roman-krysyaev <krysyaev_ry@ascon.ru>
2024-10-15 23:49:16 +0300

    Add project files.

---
+++ ShakingLight/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ShakingLight
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
